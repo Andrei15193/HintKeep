0f6710053d9507ebab9eb99078c63899185b70a3
Andrei Fangli <andrei_fangli@outlook.com>
2025-03-16 18:27:17 +0200

    Added URL rewrites to index

--- webpack.config.ts
+++ webpack.config.ts
@@ -69,6 +69,7 @@ export default function (_: any, { mode }: IBuildOptions): Configuration {
         },
         devServer: {
             open: true,
+            historyApiFallback: true,
             client: {
                 overlay: false
             }
