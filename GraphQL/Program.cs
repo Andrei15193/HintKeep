e9c0ce9469f40027965c6f17a2720e2b60d8ee26
Andrei Fangli <andrei_fangli@outlook.com>
2025-07-28 17:42:56 +0300

    Added GraphQL base project setup

---
+++ GraphQL/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication
+    .CreateBuilder(args)
+    .ConfigureFunctionsWebApplication();
+
+builder
+    .Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+builder
+    .Build()
+    .Run();
---
+++ GraphQL/Unit.Tests.cs
@@ -0,0 +1,9 @@
+﻿namespace HintKeep.GraphQL;
+
+public class UnitTestSample
+{
+    [Fact]
+    public void UnitTest()
+    {
+    }
+}
