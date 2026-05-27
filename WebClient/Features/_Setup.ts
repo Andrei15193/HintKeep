118ed89b81c968653a8fba28527f8f77fbe70882
Andrei Fangli <andrei_fangli@outlook.com>
2026-05-27 13:34:59 +0000

    Added step snapshots

--- WebClient/Features/_Setup.ts
+++ WebClient/Features/_Setup.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { After, Before, setDefaultTimeout, setWorldConstructor } from "@cucumber/cucumber";
+import { After, AfterStep, Before, BeforeAll, setDefaultTimeout, setWorldConstructor } from "@cucumber/cucumber";
 import { featureTestOptions } from "./_FeatureTestOptions";
 import { JSDomWorld } from "./_JSDomWorld";
 
@@ -8,19 +8,44 @@ import { JSDomWorld } from "./_JSDomWorld";
 setWorldConstructor(JSDomWorld);
 setDefaultTimeout(60 * 1000);
 
+BeforeAll(async function () {
+    if (featureTestOptions.failedScenarioHtmlSnapshotDirectoryPath)
+        await createDirectoryAsync(featureTestOptions.failedScenarioHtmlSnapshotDirectoryPath);
+    if (featureTestOptions.stepHtmlSnapshotDirectoryPath)
+        await createDirectoryAsync(featureTestOptions.stepHtmlSnapshotDirectoryPath);
+});
+
 Before(function () {
     this.render();
 });
 
-After(function (context) {
-    if (featureTestOptions.htmlSnapshotDirectoryPath) {
-        if (context.result)
-            this.document.head.title = `[${context.result.status}] ${this.document.head.title} (${context.pickle.name})`;
+AfterStep(async function (context) {
+    if (featureTestOptions.stepHtmlSnapshotDirectoryPath)
+        await writeFileAsync(
+            path.join(
+                featureTestOptions.stepHtmlSnapshotDirectoryPath,
+                `${context.gherkinDocument.feature} - ${context.pickle.name} - ${context.pickleStep.text}.html`
+            ),
+            this.dom.serialize()
+        );
+});
 
-        fs.mkdirSync(featureTestOptions.htmlSnapshotDirectoryPath, { recursive: true });
+After(async function (context) {
+    if (
+        context.result
+        && (
+            context.result.status === "FAILED"
+            || context.result.status === "AMBIGUOUS"
+        )
+        && featureTestOptions.failedScenarioHtmlSnapshotDirectoryPath
+    ) {
+        this.document.head.title = `[${context.result.status}] ${this.document.head.title} (${context.pickle.name})`;
 
-        fs.writeFileSync(
-            path.join(featureTestOptions.htmlSnapshotDirectoryPath, context.pickle.name + " snapshot.html"),
+        await writeFileAsync(
+            path.join(
+                featureTestOptions.failedScenarioHtmlSnapshotDirectoryPath,
+                `${context.gherkinDocument.feature?.name} - ${context.pickle.name}.html`
+            ),
             this.dom.serialize()
         );
     }
@@ -35,4 +60,30 @@ declare module "@cucumber/cucumber" {
 declare global {
     var window: Window & typeof globalThis;
     var document: Document;
+}
+
+function createDirectoryAsync(path: string): Promise<void> {
+    return new Promise<void>(
+        (resolve, reject) => {
+            fs.mkdir(path, { recursive: true }, (error) => {
+                if (error)
+                    reject(error);
+                else
+                    resolve();
+            });
+        }
+    );
+}
+
+function writeFileAsync(path: string, content: string): Promise<void> {
+    return new Promise<void>(
+        (resolve, reject) => {
+            fs.writeFile(path, content, (error) => {
+                if (error)
+                    reject(error);
+                else
+                    resolve();
+            });
+        }
+    );
 }
